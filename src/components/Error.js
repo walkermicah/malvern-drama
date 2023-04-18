f00f985800e5fed1f1ed1a5052bbb163ada71830
Walker <walker.m.oliver@gmail.com>
2023-04-17 20:17:27 -0400

    Add back button

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,14 +1,15 @@
+import { useNavigate } from 'react-router-dom';
 import '../styles/Error.scss';
 
 const Error = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="error">
       <div className="error-container">
         <h2>Oops!</h2>
         <p>The page you were looking for was not found.</p>
-        {/* To do:
-     Change error message
-     Go back */}
+        <button onClick={() => navigate(-1)}>Go back</button>
       </div>
     </div>
   );
